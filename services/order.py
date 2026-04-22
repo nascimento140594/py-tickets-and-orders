e3e9057fe3460f2484c062c98a06a8b7f677e8a2
Matheus Araujo <nascimento140594@gmail.com>
2026-04-21 22:25:31 -0300

    add missing services modules and migration

---
+++ services/order.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import MovieSession, Order, Ticket
+
+User = get_user_model()
+
+
+@transaction.atomic
+def create_order(
+    tickets: list[dict],
+    username: str,
+    date: str = None,
+) -> Order:
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+
+    if date is not None:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        order.save()
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session=MovieSession.objects.get(
+                id=ticket["movie_session"]
+            ),
+            order=order,
+        )
+
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    queryset = Order.objects.all()
+
+    if username is not None:
+        queryset = queryset.filter(user__username=username)
+
+    return queryset
