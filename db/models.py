771645ef61fdca76607906fda806300895512aa9
Matheus Araujo <nascimento140594@gmail.com>
2026-04-21 22:42:15 -0300

    fix: final corrections (models, services, flake8)

--- db/models.py
+++ db/models.py
@@ -1,121 +1,27 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
-from django.db import models
+def clean(self) -> None:
+    rows = self.movie_session.cinema_hall.rows
+    seats = self.movie_session.cinema_hall.seats_in_row
 
+    if not 1 <= self.row <= rows:
+        raise ValidationError(
+            {
+                "row": [
+                    (
+                        "row number must be in available range: "
+                        f"(1, rows): (1, {rows})"
+                    )
+                ]
+            }
+        )
 
-class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255, db_index=True)
-    description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies")
-    genres = models.ManyToManyField(Genre, related_name="movies")
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
-    @property
-    def capacity(self) -> int:
-        return self.rows * self.seats_in_row
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class MovieSession(models.Model):
-    show_time = models.DateTimeField()
-    movie = models.ForeignKey(
-        Movie,
-        on_delete=models.CASCADE,
-        related_name="sessions",
-    )
-    cinema_hall = models.ForeignKey(
-        CinemaHall,
-        on_delete=models.CASCADE,
-        related_name="sessions",
-    )
-
-    def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time}"
-
-
-class User(AbstractUser):
-    pass
-
-
-class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="orders",
-    )
-
-    class Meta:
-        ordering = ["-created_at"]
-
-    def __str__(self) -> str:
-        return str(self.created_at)
-
-
-class Ticket(models.Model):
-    movie_session = models.ForeignKey(
-        MovieSession,
-        on_delete=models.CASCADE,
-        related_name="tickets",
-    )
-    order = models.ForeignKey(
-        Order,
-        on_delete=models.CASCADE,
-        related_name="tickets",
-    )
-    row = models.IntegerField()
-    seat = models.IntegerField()
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["movie_session", "row", "seat"],
-                name="unique_ticket_for_movie_session",
-            )
-        ]
-
-    def __str__(self) -> str:
-        return f"{self.movie_session} (row: {self.row}, seat: {self.seat})"
-
-    def clean(self) -> None:
-        rows = self.movie_session.cinema_hall.rows
-        seats = self.movie_session.cinema_hall.seats_in_row
-
-        if not 1 <= self.row <= rows:
-            raise ValidationError(
-                {"row": [f"row number must be in available range: (1, rows): (1, {rows})"]}
-            )
-
-        if not 1 <= self.seat <= seats:
-            raise ValidationError(
-                {"seat": [f"seat number must be in available range: (1, seats_in_row): (1, {seats})"]}
-            )
-
-    def save(self, *args, **kwargs) -> None:
-        self.full_clean()
-        super().save(*args, **kwargs)
+    if not 1 <= self.seat <= seats:
+        raise ValidationError(
+            {
+                "seat": [
+                    (
+                        "seat number must be in available range: "
+                        f"(1, seats_in_row): (1, {seats})"
+                    )
+                ]
+            }
+        )
